0334f1042a3d7d457a8654ac6a9f79e120a3894c
xapis-21 <xapisofficial@gmail.com>
2025-08-21 06:18:00 +0300

    Update ESLint configuration to disable specific TypeScript rules and allow unescaped entities in React components.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(["next/core-web-vitals", "next/typescript"]),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react/no-unescaped-entities": 0,
+    },
+  },
 ];
 
 export default eslintConfig;
