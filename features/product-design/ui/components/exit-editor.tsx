089bc44c3ea28952bffd4e4dcba08b78bb9b2897
shafic-21 <xapisofficial@gmail.com>
2025-09-14 23:03:10 +0300

    fixed offload

--- features/product-design/ui/components/exit-editor.tsx
+++ features/product-design/ui/components/exit-editor.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
 	AlertDialog,
 	AlertDialogAction,
@@ -12,31 +14,41 @@ import {
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { useProductDesignStore } from "../../store";
 
-export const ExitEditorButton = () => (
-	<AlertDialog>
-		<AlertDialogTrigger asChild className="w-full">
-			<Button variant="outline">Back to dashboard</Button>
-		</AlertDialogTrigger>
-		<AlertDialogContent>
-			<AlertDialogHeader>
-				<AlertDialogTitle>Your are about with unsaved changes</AlertDialogTitle>
-				<AlertDialogDescription>
-					This action cannot be undone. Your changes will not be saved and you
-					will have to start over.
-				</AlertDialogDescription>
-			</AlertDialogHeader>
-			<AlertDialogFooter>
-				<AlertDialogCancel>Stay</AlertDialogCancel>
-				<AlertDialogAction asChild>
-					<Link
-						href="/creator/dashboard"
-						className={cn(buttonVariants({ variant: "destructive" }))}
-					>
-						Leave
-					</Link>
-				</AlertDialogAction>
-			</AlertDialogFooter>
-		</AlertDialogContent>
-	</AlertDialog>
-);
+export const ExitEditorButton = () => {
+	const cleanupSession = useProductDesignStore((state) => state.cleanupSession);
+
+	const handleLeave = () => {
+		cleanupSession();
+	};
+
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger asChild className="w-full">
+				<Button variant="outline">Back to dashboard</Button>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Your are about with unsaved changes</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. Your changes will not be saved and you
+						will have to start over.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Stay</AlertDialogCancel>
+					<AlertDialogAction asChild>
+						<Link
+							href="/creator/dashboard"
+							className={cn(buttonVariants({ variant: "destructive" }))}
+							onClick={handleLeave}
+						>
+							Leave
+						</Link>
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+};
